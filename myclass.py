181657fb53ca995ec4f3d69312aade536c5b2351
Quentin <quentin.cardona@efrei.net>
2023-03-13 17:34:04 +0100

    print removed

--- myclass.py
+++ myclass.py
@@ -1,35 +1,36 @@
 from pyflowchart import *
 
+
 class automata():
-    def __init__(self,data) -> None:
-        self.language:list[str] = data["language"]
-        self.states:dict[str,dict[str,list[str]]|bool] = data["states"]
-        self.init_state:str = self.findInitState()
+    def __init__(self, data) -> None:
+        self.language: list[str] = data["language"]
+        self.states: dict[str, dict[str, list[str]] | bool] = data["states"]
+        self.init_state: str = self.findInitState()
 
     def findInitState(self) -> str:
         initState = None
-        for state,properties in self.states.items():
-            if properties["start"] and initState==None:
+        for state, properties in self.states.items():
+            if properties["start"] and initState == None:
                 initState = state
             elif properties["start"] and initState:
                 return None
         return initState
 
     def ifStandard(self) -> bool:
-        if self.init_state==None:
+        if self.init_state == None:
             return False
-        for state,propreties in self.states.items():
+        for state, propreties in self.states.items():
             for letter in self.language:
                 for endState in propreties[letter]:
                     if endState == self.init_state:
                         return False
         return True
-    
+
     def standardize(self) -> None:
         if self.ifStandard():
             raise Exception("Your automata is already standard")
         transition = {}
-        for state,properties in self.states.items():
+        for state, properties in self.states.items():
             if properties["start"]:
                 properties["start"] = False
                 for letter in self.language:
@@ -42,31 +43,52 @@ class automata():
         self.states["i"] = state
         self.init_state = "i"
 
-    #TODO : Menu principal - Soizic
+    # TODO : Menu principal - Soizic
 
-    #TODO : Display - Paul
+    # TODO : Display - Paul
 
-    #TODO : Minimize - Jade
+    # TODO : Minimize - Jade
 
-    #TODO : Complement + Word recognition - Axel
+    # TODO : Complement + Word recognition - Axel
 
-    #TODO: Determinize + complete - Quentin
+    # TODO: Determinize + complete - Quentin
+    def complete(self) -> None:
+        create = True
+        for states in self.states:
+            if ("P" in states):
+                create = False
+        if create:
+            P = {}
+            for letter in self.language:
+                P[letter] = ["P"]
+            p_states = {
+                "start": False,
+                "end": False,
+            }
+            P.update(p_states)
+            self.states["P"] = P
+
+        for state, properties in self.states.items():
+            for letter in self.language:
+                if (properties[letter] == []):
+                    properties[letter] = ["P"]
 
     def export(self) -> str:
         if not self.init_state:
-            raise Exception("Your automata must at least be standard to be export.")
-        
-        nodes:dict[str,OperationNode] = {}
+            raise Exception(
+                "Your automata must at least be standard to be export.")
 
-        for state,property in self.states.items():
+        nodes: dict[str, OperationNode] = {}
+
+        for state, property in self.states.items():
             if property["start"]:
                 nodes[state] = StartNode(state)
-            elif property ["end"]:
+            elif property["end"]:
                 nodes[state] = EndNode(state)
             else:
                 nodes[state] = OperationNode(state)
 
-        for state,propreties in self.states.items():
+        for state, propreties in self.states.items():
             for letter in self.language:
                 for endState in propreties[letter]:
                     nodes[state].connect(nodes[endState])
