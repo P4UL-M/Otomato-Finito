89b33dd6ed5fdc528f5dafccd7478ccbbd4e76f0
AxelLns <axel.loones@gmail.com>
2023-03-15 11:46:12 +0100

    Finito Otomata!

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pyflowchart import *
 from myclass import automata
 
 # Load the JSON file
-with open('exemple_data_format.json') as f:
+with open('FA/Int1-2-44.json') as f:
     data = json.load(f)
 
 myAutomata = automata(data)
@@ -12,7 +12,7 @@ myAutomata = automata(data)
 # except Exception as err:
 #     print(err)
 
-print(myAutomata.recognize('AAAAAA'))
+# print(myAutomata.recognize('AAAAAA'))
 #print(myAutomata.isComplete())
 #myAutomata.complete()
 myAutomata.display(1)
