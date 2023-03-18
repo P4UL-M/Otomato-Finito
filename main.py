ab3768a5cc227e600e9702109eddc8a66334de55
GitSnows <91462454+GitSnows@users.noreply.github.com>
2023-03-18 15:41:04 +0100

    Addition of the menu

--- main.py
+++ main.py
@@ -2,24 +2,74 @@ import json
 from pyflowchart import *
 from myclass import automata, stateName
 from pathlib import Path
+from InquirerPy import inquirer
+from pathlib import Path
+import os
 
 path = Path(__file__).parent
 
-# Load the JSON file
-with open(path / 'FA' / "Int1-2-22.json", encoding="utf-8") as f:
-    data = json.load(f)
+if __name__ == '__main__':
+    menu_on = True
+    while menu_on == True :
 
-myAutomata = automata(data)
-myAutomata.display(1)
-if not myAutomata.isStandard():
-    myAutomata.standardize()
-if not myAutomata.isDeterministic():
-    myAutomata.determinize()
-if not myAutomata.isComplete():
-    myAutomata.complete()
-if not myAutomata.isAsync:
-    myAutomata.complementary()
-myAutomata.display(1)
-print("recognize :",myAutomata.recognize("AAAABBBBBBBBBBBBBBB"))
+        file_chosen = inquirer.fuzzy(
+            message="Which file would you like to import :",
+            choices=os.listdir("FA"),
+            default="Int1-2-",
+        ).execute()
+
+        action = inquirer.select(
+            message="What do you want to do ?",
+            choices=["Displaying FA", "Standardization", "Determinization", "Completion", "Minimization",
+                     "Word recognition", "Complementary language"],
+        ).execute()
+        # confirm = inquirer.confirm(message="Confirm?").execute()
+        # if confirm == True :
+        with open(path / 'FA' / file_chosen, encoding="utf-8") as f:
+            data = json.load(f)
+        myAutomata = automata(data)
+
+        if action == "Displaying FA":
+            myAutomata.display(1)
+
+        elif action == "Standardization":
+            if not myAutomata.isStandard():
+                myAutomata.standardize()
+                print("Standardization completed !")
+                myAutomata.display(1)
+            else:
+                print("The automata is already standard.")
+
+        elif action == "Determinization":
+            if not myAutomata.isDeterministic():
+                myAutomata.determinize()
+                print("Determinization completed !")
+                myAutomata.display(1)
+            else:
+                print("The automata is already deterministic.")
+
+        elif action == "Completion":
+            if not myAutomata.isComplete():
+                myAutomata.complete()
+                print("Completion completed !")
+                myAutomata.display(1)
+            else:
+                print("The automata is already complete.")
+
+        elif action == "Minimization":
+            pass
+
+        elif action == "Word recognition":
+            print("recognize :",myAutomata.recognize("AAAABBBBBBBBBBBBBBB"))
+
+        elif action == "Complementary language":
+            if not myAutomata.isAsync:
+                myAutomata.complementary()
+                print("Here is the complement automata :")
+                myAutomata.display(1)
+
+        menu_on = inquirer.confirm(message="Do you want to continue ?").execute()
+
 #print(myAutomata.isComplete())
 #myAutomata.complete()
+
